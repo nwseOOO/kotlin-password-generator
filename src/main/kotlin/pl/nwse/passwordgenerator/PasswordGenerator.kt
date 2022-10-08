6f26b5924a226972e46c09bc3ea84b4652160386
nwseUwU <nwse@nwse.pl>
2022-10-08 19:14:13 +0200

    Initial commit

---
+++ src/main/kotlin/pl/nwse/passwordgenerator/PasswordGenerator.kt
@@ -0,0 +1,34 @@
+package pl.nwse.passwordgenerator
+
+import org.apache.commons.lang3.RandomStringUtils
+import java.security.SecureRandom
+
+class PasswordGenerator {
+
+    private val letters : String = "abcdefghijklmnopqrstuvwxyz"
+    private val numbers : String = "1234567890"
+    private val characters : String = "~`!@#\$%^&*()-_=+[{]}\\|;:'\",<.>/?";
+
+    private val pattern : CharArray = (this.letters + this.letters.uppercase() + this.numbers + this.characters).toCharArray()
+
+    fun generatePassword() : String {
+        return RandomStringUtils.random(
+            32,
+            0,
+            this.pattern.size.minus(1),
+            false,
+            false,
+            this.pattern,
+            SecureRandom()
+        )
+    }
+
+}
+
+fun main() {
+    val passwordGenerator : PasswordGenerator = PasswordGenerator()
+
+    for(i in 1..10) {
+        println("%d. ".format(i) + passwordGenerator.generatePassword())
+    }
+}
